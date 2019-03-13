70ea94dd20545b95f144edbf0d77e06902c60270
Jackie Li <jli38@stuy.edu>
2019-03-13 03:41:11 -0400

    2

--- main.py
+++ main.py
@@ -9,6 +9,16 @@ color = [ 0, 255, 0 ]
 edges = []
 transform = new_matrix()
 
+/*
+bezier
+80 235 80 20 475 486 425 425
+bezier
+80 235 95 140 475 486 425 425
+display
+save
+test.png
+*/
+
 print_matrix(make_bezier())
 print_matrix(make_hermite())
 
